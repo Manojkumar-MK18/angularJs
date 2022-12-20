9ad8a11169ce2db4002d0ab2cb3bfe9927ceb0ee
MK <manojkumar.sekar2000@gmail.com>
2022-12-20 16:59:52 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/Task/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/Task/tasks/tasks.component';
 import { TasksItemComponent } from './components/Task/tasks-item/tasks-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AddTaskComponent } from './components/Task/add-task/add-task.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
 import { ReducerComponent } from './components/reducer/reducer.component';
 

1eb11a2038b1b6fbc0bb0c402d1ad5b0ac9b0fd1
MK <manojkumar.sekar2000@gmail.com>
2022-12-20 17:09:26 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReducerComponent } from './components/reducer/reducer.component';
+import { TasksComponent } from './components/Task/tasks/tasks.component';
 
-const routes: Routes = [{ path: 'reducer', component: ReducerComponent }];
+const routes: Routes = [
+  { path: 'reducer', component: ReducerComponent },
+  { path: 'tasks', component: TasksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }

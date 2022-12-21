b17fd81bbb027f0d874ebb408c4ccac9b720952c
MK <manojkumar.sekar2000@gmail.com>
2022-12-21 14:32:32 +0530

    add

--- src/app/components/movieapp/login/login.component.ts
+++ src/app/components/movieapp/login/login.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  userName: string = '';
+  password: any = '';
 
+  handleLogin() {
+    console.log(this.password);
+    console.log(this.userName);
+    this.userName = ''
+    this.password = ''
+  }
+
+  ngOnInit() {
+  }
 }

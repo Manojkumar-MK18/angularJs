98a5180fedd1159a3e19986b8da104d848a045fd
MK <manojkumar.sekar2000@gmail.com>
2022-12-21 15:56:53 +0530

    add

--- src/app/components/movieapp/moviepage/moviepage.component.ts
+++ src/app/components/movieapp/moviepage/moviepage.component.ts
@@ -1,10 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-moviepage',
   templateUrl: './moviepage.component.html',
-  styleUrls: ['./moviepage.component.css']
+  styleUrls: ['./moviepage.component.css'],
 })
 export class MoviepageComponent {
+  treandingMoviesList: any = [];
 
+  constructor(public http: HttpClient) {}
+  ngOnInit() {
+    this.getTreandingMoviesList();
+  }
+  getTreandingMoviesList() {
+    this.http
+      .get('http://localhost:4200/assets/data/trending-movies.json')
+      .subscribe((list) => (this.treandingMoviesList = list));
+  }
 }
